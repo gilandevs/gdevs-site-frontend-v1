0df437d47cfe67911f3434a65be6eec013dd18db
Arash <arashmilani22@gmail.com>
2022-07-22 21:40:04 +0430

    finsh suport page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,124 +1,164 @@
-(function () {
-  var imageGrid = document.querySelector(".img-grid");
-  if (!imageGrid) {
-    return;
-  }
-  var members = [
-    {
-      avatar: "assets/img/avatars/avatar-1.jpg",
-      name: "mohammad kamalipour",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-2.jpg",
-      name: "arad",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-3-1.jpg",
-      name: "sina",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-4.jpg",
-      name: "Arash Milani",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-5.jpg",
-      name: "Hassan",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-6.jpg",
-      name: "Aryan",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-7.jpg",
-      name: "Samira",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-8.jpg",
-      name: "ali",
-    },
-    {
-      avatar: "assets/img/avatars/avatar-9.jpg",
-      name: "hossein",
-    },
-  ];
-  var parallax = ["-30", "-30", "-45", "-50", "-20", "-15", "-10"];
+document.addEventListener("DOMContentLoaded", function () {
+  (function () {
+    var button = document.querySelector(".download-images");
 
-  var shuffleArray = (arr) =>
-    arr.sort(function () {
-      return 0.5 - Math.random();
-    });
-
-  var shuffleMembers = shuffleArray(members);
-  var maxImage = 7;
-  counter = 1;
-
-  while (counter <= maxImage) {
-    const imgWrapper = document.createElement("div");
-    const avatar = document.createElement("img");
-    imgWrapper.className = "img-grid-item";
-
-    avatar.src = shuffleMembers[counter].avatar;
-    avatar.alt = shuffleMembers[counter].alt;
-    avatar.dataset.jarallaxElement = parallax[counter];
-    if (counter === 1) {
-      avatar.className =
-        "img-fluid rounded-top-start-3 rounded-bottom-end-3 shadow-lg";
-    } else {
-      avatar.className =
-        "img-fluid rounded-top-start rounded-bottom-end shadow-lg";
+    if (!button) {
+      return null;
     }
 
-    imgWrapper.appendChild(avatar);
-    imageGrid.appendChild(imgWrapper);
-    counter++;
-  }
-})();
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      window.open("/storage/img/instagram/Instagram-story-1.png");
+      window.open("/storage/img/instagram/Instagram-story-2.png");
+      window.open("/storage/img/instagram/Instagram-story-3.png");
+    });
+  })();
+  (function () {
+    var imageGrid = document.querySelector(".img-grid");
+    if (!imageGrid) {
+      return;
+    }
+    var members = [
+      {
+        avatar: "assets/img/avatars/avatar-1.jpg",
+        name: "mohammad kamalipour",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-2.jpg",
+        name: "arad",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-3-1.jpg",
+        name: "sina",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-4.jpg",
+        name: "Arash Milani",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-5.jpg",
+        name: "Hassan",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-6.jpg",
+        name: "Aryan",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-7.jpg",
+        name: "Samira",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-8.jpg",
+        name: "ali",
+      },
+      {
+        avatar: "assets/img/avatars/avatar-9.jpg",
+        name: "hossein",
+      },
+    ];
+    var parallax = ["-30", "-30", "-45", "-50", "-20", "-15", "-10"];
 
-const formEl = document.getElementById("my_form");
-const nameInput = document.getElementById("name");
-const emailInput = document.getElementById("email");
-const messageInput = document.getElementById("message");
-const submitBtn = document.getElementById("submitform");
-const formSubmitHandler = (e) => {
-  e.preventDefault();
-  submitBtn.classList.add("loading");
-  submitBtn.setAttribute("disabled", "");
-  const name = formEl.elements["name"].value;
-  const email = formEl.elements["email"].value;
-  const message = formEl.elements["message"].value;
-  var formdata = new FormData();
-  formdata.append("name", name);
-  formdata.append("email", email);
-  formdata.append("message", message);
+    var shuffleArray = (arr) =>
+      arr.sort(function () {
+        return 0.5 - Math.random();
+      });
 
-  var ajax = new XMLHttpRequest();
-  ajax.open("POST", "/service/mailer.php");
-  ajax.onreadystatechange = function () {
-    if (ajax.readyState == 4 && ajax.status == 200) {
-      if (ajax.responseText == "success") {
+    var shuffleMembers = shuffleArray(members);
+    var maxImage = 7;
+    counter = 1;
+
+    while (counter <= maxImage) {
+      const imgWrapper = document.createElement("div");
+      const avatar = document.createElement("img");
+      imgWrapper.className = "img-grid-item";
+
+      avatar.src = shuffleMembers[counter].avatar;
+      avatar.alt = shuffleMembers[counter].alt;
+      avatar.dataset.jarallaxElement = parallax[counter];
+      if (counter === 1) {
+        avatar.className =
+          "img-fluid rounded-top-start-3 rounded-bottom-end-3 shadow-lg";
+      } else {
+        avatar.className =
+          "img-fluid rounded-top-start rounded-bottom-end shadow-lg";
+      }
+
+      imgWrapper.appendChild(avatar);
+      imageGrid.appendChild(imgWrapper);
+      counter++;
+    }
+  })();
+
+
+  (function(){
+    const formEl = document.getElementById("my_form");
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+    const submitBtn = document.getElementById("submitform");
+  
+    if(!formEl){
+      return null
+    }
+    const formSubmitHandler = (e) => {
+      e.preventDefault();
+      submitBtn.classList.add("loading");
+      submitBtn.setAttribute("disabled", "");
+      const name = formEl.elements["name"].value;
+      const email = formEl.elements["email"].value;
+      const message = formEl.elements["message"].value;
+      var formdata = new FormData();
+      formdata.append("name", name);
+      formdata.append("email", email);
+      formdata.append("message", message);
+  
+      var ajax = new XMLHttpRequest();
+      ajax.open("POST", "/service/mailer.php");
+      ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4 && ajax.status == 200) {
+          if (ajax.responseText == "success") {
+            submitBtn.classList.remove("loading");
+            submitBtn.removeAttribute("disabled");
+  
+            Toastify({
+              text: "پیام شما با موفقیت ارسال شد.",
+              duration: 3000,
+              destination: "https://github.com/apvarun/toastify-js",
+              newWindow: true,
+              close: false,
+              gravity: "top", // `top` or `bottom`
+              position: "left", // `left`, `center` or `right`
+              stopOnFocus: true, // Prevents dismissing of toast on hover
+              style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+              },
+              onClick: function () {}, // Callback after click
+            }).showToast();
+          } else if (ajax.responseText == "error") {
+            submitBtn.classList.remove("loading");
+            submitBtn.removeAttribute("disabled");
+  
+            Toastify({
+              text: "نشد که بشه! ‌:(",
+              duration: 3000,
+              close: false,
+              gravity: "top",
+              position: "right",
+              stopOnFocus: true,
+              style: {
+                background:
+                  "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
+              },
+            }).showToast();
+          }
+        }
+      };
+      if (nameInput.value === "") {
         submitBtn.classList.remove("loading");
         submitBtn.removeAttribute("disabled");
-
+  
         Toastify({
-          text: "پیام شما با موفقیت ارسال شد.",
-          duration: 3000,
-          destination: "https://github.com/apvarun/toastify-js",
-          newWindow: true,
-          close: false,
-          gravity: "top", // `top` or `bottom`
-          position: "left", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          style: {
-            background: "linear-gradient(to right, #00b09b, #96c93d)",
-          },
-          onClick: function () {}, // Callback after click
-        }).showToast();
-      } else if (ajax.responseText == "error") {
-        submitBtn.classList.remove("loading");
-        submitBtn.removeAttribute("disabled");
-
-        Toastify({
-          text: "نشد که بشه! ‌:(",
+          text: "اسمتو وارد نکردیا!",
           duration: 3000,
           close: false,
           gravity: "top",
@@ -129,61 +169,47 @@ const formSubmitHandler = (e) => {
               "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
           },
         }).showToast();
+        return null;
       }
-    }
-  };
-  if (nameInput.value === "") {
-    submitBtn.classList.remove("loading");
-    submitBtn.removeAttribute("disabled");
-
-    Toastify({
-      text: "اسمتو وارد نکردیا!",
-      duration: 3000,
-      close: false,
-      gravity: "top",
-      position: "right",
-      stopOnFocus: true,
-      style: {
-        background: "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
-      },
-    }).showToast();
-    return null;
-  }
-  if (emailInput.value === "") {
-    submitBtn.classList.remove("loading");
-    submitBtn.removeAttribute("disabled");
-
-    Toastify({
-      text: "ایمیلتو وارد نکردیا!",
-      duration: 3000,
-      close: false,
-      gravity: "top",
-      position: "right",
-      stopOnFocus: true,
-      style: {
-        background: "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
-      },
-    }).showToast();
-    return null;
-  }
-  if (messageInput.value === "") {
-    submitBtn.classList.remove("loading");
-    submitBtn.removeAttribute("disabled");
-
-    Toastify({
-      text: "پیامتو وارد نکردیا!",
-      duration: 3000,
-      close: false,
-      gravity: "top",
-      position: "right",
-      stopOnFocus: true,
-      style: {
-        background: "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
-      },
-    }).showToast();
-    return null;
-  }
-  ajax.send(formdata);
-};
-
-formEl.addEventListener("submit", formSubmitHandler);
+      if (emailInput.value === "") {
+        submitBtn.classList.remove("loading");
+        submitBtn.removeAttribute("disabled");
+  
+        Toastify({
+          text: "ایمیلتو وارد نکردیا!",
+          duration: 3000,
+          close: false,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background:
+              "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
+          },
+        }).showToast();
+        return null;
+      }
+      if (messageInput.value === "") {
+        submitBtn.classList.remove("loading");
+        submitBtn.removeAttribute("disabled");
+  
+        Toastify({
+          text: "پیامتو وارد نکردیا!",
+          duration: 3000,
+          close: false,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+          style: {
+            background:
+              "linear-gradient(to right, rgb(176 0 69), rgb(201 115 61))",
+          },
+        }).showToast();
+        return null;
+      }
+      ajax.send(formdata);
+    };
+  
+    formEl.addEventListener("submit", formSubmitHandler);
+  })();
+});
